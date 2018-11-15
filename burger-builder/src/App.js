55ce8f47269c42192c2c187907645ae0b9b0fd0f
Richard Horrocks <richardjhorrocks@gmail.com>
2018-11-15 19:22:00 +0000

    Component layout.

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+// This is the root component of our application.
+// As an alternative, we could just use the Layout component as the root
+// component by including it in index.js.
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
