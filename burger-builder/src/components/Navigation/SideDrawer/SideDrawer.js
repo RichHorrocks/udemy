1dbad88d03fe2a309a1aed3a286f3ae376de33ba
Richard Horrocks <richardjhorrocks@gmail.com>
2018-11-26 15:47:35 +0000

    Toggle button.

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,28 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
-const sideDrawer = () => {
+const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+
+  if (props.isOpen) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show={props.isOpen} clicked={props.closed}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
