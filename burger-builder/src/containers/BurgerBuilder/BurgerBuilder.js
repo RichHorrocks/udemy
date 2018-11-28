95e997aba9e9b4f549b76186b8353b8b5328e6bb
Richard Horrocks <richardjhorrocks@gmail.com>
2018-11-28 18:20:20 +0000

    Completion of main site.

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux/Aux';
 
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
@@ -34,8 +34,6 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-
-    console.log(sum);
 
     this.setState({ purchaseable: sum > 0 });
   }
