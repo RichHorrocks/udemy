a6b7bab95f6f0971b8b2e6891b24a242ceaffcb5
Richard Horrocks <richardjhorrocks@gmail.com>
2018-12-08 21:15:30 +0000

    Added orders page.

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 // This is the root component of our application.
 // As an alternative, we could just use the Layout component as the root
@@ -15,6 +16,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import Order from '../../components/Order/Order';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then(res => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch(() => {
+        this.setState({ loading: false });
+      });
+  }
+
   render () {
-    return ();
+    return (
+      <div>
+        <Order />
+        <Order />
+      </div>
+    );
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
