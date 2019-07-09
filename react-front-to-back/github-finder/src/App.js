b537e90313231552dc71fd7f53a70fad42fed602
Richard Horrocks <richardjhorrocks@gmail.com>
2019-07-09 21:08:46 +0100

    Addition of state.

--- react-front-to-back/github-finder/src/App.js
+++ react-front-to-back/github-finder/src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 import Navbar from './components/layout/Navbar';
 
 class App extends Component {
-  static defaultProps = {
-    title: 'Github Finder',
-    icon: 'fab fa-github'
-  };
-
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
-
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
+        <UserItem />
       </div>
     );
   }
