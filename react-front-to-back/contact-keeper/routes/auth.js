83712258ac9d34b503ce40fdb7159c31cfc968ec
Richard Horrocks <richardjhorrocks@gmail.com>
2019-08-19 10:19:35 +0100

    Middleware and Contacts routes.

--- react-front-to-back/contact-keeper/routes/auth.js
+++ react-front-to-back/contact-keeper/routes/auth.js
@@ -4,14 +4,21 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 const User = require('../models/User');
 
 // @route   GET api/auth
 // @desc    Get the logged-in user
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get a logged-in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 // @route   POST api/auth
